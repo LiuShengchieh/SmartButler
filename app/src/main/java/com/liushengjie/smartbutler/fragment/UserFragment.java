e8303831abe7b22f3ebacd724034d55414b30012
Shengjie <liushengchieh@gmail.com>
2017-10-04 22:01:07 +0800

    圆形头像

--- app/src/main/java/com/liushengjie/smartbutler/fragment/UserFragment.java
+++ app/src/main/java/com/liushengjie/smartbutler/fragment/UserFragment.java
@@ -2,9 +2,15 @@ package com.liushengjie.smartbutler.fragment;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +22,23 @@ import android.widget.Toast;
 import com.liushengjie.smartbutler.R;
 import com.liushengjie.smartbutler.entity.MyUser;
 import com.liushengjie.smartbutler.ui.LoginActivity;
+import com.liushengjie.smartbutler.utils.L;
+import com.liushengjie.smartbutler.utils.UtilTools;
+import com.liushengjie.smartbutler.view.CustomDialog;
+
+import java.io.File;
+import java.io.IOException;
 
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.UpdateListener;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
+ * 个人中心
  */
+
 public class UserFragment extends Fragment implements View.OnClickListener {
 
     private Button btn_exit_user;
@@ -33,6 +48,11 @@ public class UserFragment extends Fragment implements View.OnClickListener {
     private EditText et_age;
     private EditText et_desc;
     private Button btn_update_ok;
+    private CircleImageView profile_image;
+    private CustomDialog dialog;
+    private Button btn_camera;
+    private Button btn_picture;
+    private Button btn_cancel;
 
     public UserFragment() {
         // Required empty public constructor
@@ -62,6 +82,28 @@ public class UserFragment extends Fragment implements View.OnClickListener {
         et_sex = view.findViewById(R.id.et_sex);
         et_age = view.findViewById(R.id.et_age);
         et_desc = view.findViewById(R.id.et_desc);
+
+        //圆形头像
+        profile_image = (CircleImageView) view.findViewById(R.id.profile_image);
+        profile_image.setOnClickListener(this);
+
+        //读取图片
+        UtilTools.getImageFromShare(getActivity(), profile_image);
+
+        //dialog
+        dialog = new CustomDialog(getActivity(), 100, 100, R.layout.dialog_photo, R.style.Theme_dialog,
+                Gravity.BOTTOM, R.style.pop_anim_style);
+        //屏幕外点击无效
+        dialog.setCancelable(false);
+
+        btn_camera = (Button) dialog.findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(this);
+
+        btn_picture = (Button) dialog.findViewById(R.id.btn_picture);
+        btn_picture.setOnClickListener(this);
+
+        btn_cancel = (Button) dialog.findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(this);
 
         //默认不可点击和输入
         setEnabled(false);
@@ -145,8 +187,138 @@ public class UserFragment extends Fragment implements View.OnClickListener {
                     Toast.makeText(getActivity(), "输入框不能为空", Toast.LENGTH_SHORT).show();
                 }
                 break;
+            case R.id.profile_image:
+                dialog.show();
+                break;
+            case R.id.btn_camera:
+                toCamera();
+                break;
+            case R.id.btn_picture:
+                toPicture();
+                break;
+            case R.id.btn_cancel:
+                dialog.dismiss();
+                break;
         }
     }
 
+    public static final String PHOTO_IMAGE_FILE_NAME = "fileImg.jpg";
+    public static final int CAMERA_REQUEST_CODE = 100;
+    public static final int IMAGE_REQUEST_CODE = 101;
+    public static final int RESULT_REQUEST_CODE = 102;
+    private File tempFile = null;
+    private Uri imageUri;
 
+    //跳转相机
+    private void toCamera() {
+
+        File outputImage = new File(getActivity().getExternalCacheDir(),
+                PHOTO_IMAGE_FILE_NAME);
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(getActivity(),
+                    "com.liushengjie.smartbutler.fileprovider", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        //启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        dialog.dismiss();
+
+
+        /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //判断内存卡是否可用，可用的话进行存储
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(new File(Environment.getExternalStorageDirectory(), PHOTO_IMAGE_FILE_NAME)));
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        dialog.dismiss();*/
+
+    }
+
+    //跳转相册
+    private void toPicture() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_REQUEST_CODE);
+        dialog.dismiss();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != getActivity().RESULT_CANCELED) {
+            switch (requestCode) {
+                //相机数据
+                case CAMERA_REQUEST_CODE:
+                    //tempFile = new File(Environment.getExternalStorageDirectory(), PHOTO_IMAGE_FILE_NAME);
+                    //startPhotoZoom(Uri.fromFile(tempFile));
+                    startPhotoZoom(imageUri);
+                    break;
+                //相册数据
+                case IMAGE_REQUEST_CODE:
+                    startPhotoZoom(data.getData());
+                    break;
+                case RESULT_REQUEST_CODE:
+                    //有可能点击舍弃
+                    if (data != null) {
+                        //拿到图片设置
+                        setImageToView(data);
+                        //删除原来的图片
+                        if (tempFile != null) {
+                            tempFile.delete();
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    //裁剪
+    private void startPhotoZoom(Uri uri) {
+        if (uri == null) {
+            L.e("uri == null");
+            return;
+        }
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        //裁剪
+        intent.putExtra("crop", true);
+        //宽高比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        //图片质量
+        intent.putExtra("outputX", 320);
+        intent.putExtra("outputY", 320);
+        //发送数据
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, RESULT_REQUEST_CODE);
+    }
+
+    //设置图片
+    private void setImageToView(Intent data) {
+        Bundle bundle = data.getExtras();
+        if (bundle != null) {
+            Bitmap bitmap = bundle.getParcelable("data");
+            profile_image.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //保存图片到shareutils
+        UtilTools.putImageToShare(getActivity(), profile_image);
+    }
 }
--- app/src/main/java/com/liushengjie/smartbutler/utils/UtilTools.java
+++ app/src/main/java/com/liushengjie/smartbutler/utils/UtilTools.java
@@ -1,8 +1,16 @@
 package com.liushengjie.smartbutler.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Base64;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 /**
  * 项目名：SmartButler
@@ -18,6 +26,34 @@ public class UtilTools {
     public static void setFont(Context mContent, TextView textView) {
         Typeface fontType = Typeface.createFromAsset(mContent.getAssets(), "fonts/FONT.TTF");
         textView.setTypeface(fontType);
+    }
+
+    //保存图片到shareutils
+    public static void putImageToShare(Context mContext, ImageView imageView) {
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        //第一步：将bitmap压缩成字节数组输出流
+        ByteArrayOutputStream byStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byStream);
+        //第二步：利用base64将字节数组输出流转换为string
+        byte[] byteArray = byStream.toByteArray();
+        String imgString = new String(Base64.encodeToString(byteArray, Base64.DEFAULT));
+        //第三步：将string保存到shareutils
+        ShareUtils.putString(mContext, "image_title", imgString);
+    }
+
+    //读取图片
+    public static void getImageFromShare(Context mContext, ImageView imageView) {
+        //第一步：拿到string
+        String imgString = ShareUtils.getString(mContext, "image_title", "");
+        if (!imgString.equals("")) {
+            //第二步：通过base64将string转为字节数组
+            byte[] byteArray = Base64.decode(imgString, Base64.DEFAULT);
+            ByteArrayInputStream byStream = new ByteArrayInputStream(byteArray);
+            //第三步：生成bitmap
+            Bitmap bitmap = BitmapFactory.decodeStream(byStream);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 
 }
